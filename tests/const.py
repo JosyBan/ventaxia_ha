90e0ca295544c957b1a3c8a1c388fa8df5411d6a
JosyBan <joe_bob_mac@yahoo.com>
2025-07-27 16:40:23 +0100

    update const.py

--- tests/const.py
+++ tests/const.py
@@ -1,9 +1,9 @@
-"""Constants for {{cookiecutter.friendly_name}} tests."""
-from custom_components.{{cookiecutter.domain_name}}.const import (
-    CONF_PASSWORD,
+"""Constants for ventaxia_ha tests."""
+from custom_components.ventaxia_ha.const import (
+    CONF_IDENTITY,
 )
-from custom_components.{{cookiecutter.domain_name}}.const import (
-    CONF_USERNAME,
+from custom_components.ventaxia_ha.const import (
+    CONF_PSK_KEY,
 )
 
-MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
+MOCK_CONFIG = {CONF_IDENTITY: "test_username", CONF_PSK_KEY: "test_password"}
